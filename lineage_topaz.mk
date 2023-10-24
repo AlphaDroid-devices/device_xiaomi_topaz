acbe94448d069d5497d66df6b4b66949c31b2903
rapiii <fatsurbaby@gmail.com>
2023-10-24 11:33:43 +0000

    topaz: AlphaDroid Bringup

--- lineage_topaz.mk
+++ lineage_topaz.mk
@@ -8,18 +8,36 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common AlphaDroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from topaz device.
 $(call inherit-product, device/xiaomi/topaz/device.mk)
 
-# Flags rom
+# Device Config
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
-TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GAPPS := false
+TARGET_INCLUDE_GOOGLE_TELEPHONY := false
+TARGET_INCLUDE_PIXEL_FRAMEWORK := false
+TARGET_INCLUDE_GOOGLE_CAMERA := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_INCLUDE_GMAIL := false
+TARGET_INCLUDE_GOOGLE_MAPS := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+TARGET_DEFAULT_ADB_ENABLED := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := hefatss
 
 # Device identifier
 PRODUCT_NAME := lineage_topaz
