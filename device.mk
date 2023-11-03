18a189fb510c606a64a11bbebacd8df4e64d5d07
rapiii <fatsurbaby@gmail.com>
2023-11-03 07:15:53 +0700

    topaz: Update Powerhal to 1.3

--- device.mk
+++ device.mk
@@ -194,7 +194,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
+    android.hardware.power@1.3.vendor \
     libgrpc++_unsecure.vendor
 
 # Properties
